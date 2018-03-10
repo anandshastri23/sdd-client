08aabfd529a47e4eaa6124c6d1a10277694abdc6
Anand Shastri <anandshastri23@gmail.com>
2018-03-10 12:03:23 -0600

    server connection established!

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import axios from 'axios'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor () {
+        super();
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick () {
+        axios.get("http://localhost:8080/hello")
+            .then(response => console.log(response))
+    }
+
+    render() {
+        return (
+            <div className='button__container'>
+                <button className='button' onClick={this.handleClick}>
+                    Click Me
+                </button>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
